91146aec2dcea279d1ad3f316f9fe1e13b9edc42
José Malcher Junior <contato@josemalcher.net>
2017-04-16 15:16:45 -0300

    Aulas Python - 003 - A Função print

---
+++ parte01introducao/aula03FuncaoPrint.py
@@ -0,0 +1,22 @@
+# Aulas Python - 003 - A Função print
+# https://www.youtube.com/watch?v=RLK2x7PbG_I&index=4&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe
+
+print("Olá Mundo")
+
+print("23")
+
+print(23)
+
+print("Esse e o programa", 2)
+
+print("Eu tenho",11,"cachorros")
+
+print("Olá MUndo \n Pulando outra  lina")
+
+help(print)
+
+
+
+
+
+
