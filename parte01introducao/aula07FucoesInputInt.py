2418a3c38b740078f2ab45b0cd48c4a9f7afd226
José Malcher Junior <contato@josemalcher.net>
2017-04-16 16:38:33 -0300

    Aulas Python - 007 - As Funções input e int

---
+++ parte01introducao/aula07FucoesInputInt.py
@@ -0,0 +1,19 @@
+# Aulas Python - 007 - As Funções input e int
+# https://www.youtube.com/watch?v=75bSJZPADH0&index=8&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe
+
+num = 30
+x = 17
+print(num - x)
+
+# input("Quantos Cachorros??")
+
+x = input("Quantos cachorros?")
+print(x)
+print(type(x))
+
+x = int(input("DIGITE A QUANTIDADE"))
+print(x)
+print(type(x))
+
+
+
