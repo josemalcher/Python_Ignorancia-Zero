02068909c1d5cce3d5a028bec72cfd4c9b596ba3
José Malcher Junior <contato@josemalcher.net>
2017-04-16 15:25:14 -0300

    Aulas Python - 004 - Comentários

---
+++ parte01introducao/aula02Comentarios.py
@@ -0,0 +1,14 @@
+# Aulas Python - 004 - Comentários
+# https://www.youtube.com/watch?v=g4DemNwPdFc&index=5&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe
+
+print("Aula Sobre comentários")
+
+# Uma lina
+"""
+Comentários
+de 
+Várias LInhas
+
+"""
+
+print("Fim da aula")
