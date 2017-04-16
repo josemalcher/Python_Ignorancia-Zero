8a930519c7bc88a2ce8deafa83de1364f38a13be
José Malcher Junior <contato@josemalcher.net>
2017-04-16 17:58:42 -0300

    Aulas Python - 008 - A Variável Booleana, Operadores de Comparação

---
+++ parte01introducao/aula08VarBooleanOperComp.py
@@ -0,0 +1,33 @@
+# Aulas Python - 008 - A Variável Booleana, Operadores de Comparação
+# https://www.youtube.com/watch?v=If7qgG5Qfro&index=9&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe
+from builtins import print
+
+x = True
+y = False
+
+print(x)
+print(y)
+
+idade = int(input("QUal sua Idade?"))
+print(idade >= 18)
+
+print(idade < 20)
+
+print(idade == 18)
+
+print(idade != 18)
+
+print(True == 1)
+
+print(False == 0)
+
+# vazio -> None, False, [],{},'',"", 0, 0.0
+
+print( bool(217))
+print( bool(''))
+
+
+
+
+
+
