8c072e99b88c2b2c5f4df78e71b23b465b0f8394
José Malcher Junior <contato@josemalcher.net>
2017-04-16 16:17:09 -0300

    Aulas Python - 006 - Operações Matemáticas

---
+++ parte01introducao/aula06OperacoesMatematicas.py
@@ -0,0 +1,45 @@
+# Aulas Python - 006 - Operações Matemáticas
+# https://www.youtube.com/watch?v=MHpQugpJrTc&index=7&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe
+from builtins import print
+from re import sub
+
+x = 10;
+print("Eu tenho ", x)
+
+x = 10
+y = 6
+z = 90
+
+soma = x + y
+print(soma)
+
+subtracao = z - 40
+print(subtracao)
+
+multiplic = x * 2
+print(multiplic)
+
+#divisão
+
+divi  = z / x
+print(divi)
+
+divi_inteiro = z // x
+print(divi_inteiro)
+
+# resto
+divi_modulo = x % y
+print(divi_modulo)
+
+# Potencia
+
+potencia = x ** 2
+print(potencia)
+
+# * / // ** >> + e -
+prec = 10 + 5 * 3 / 15
+print(prec)
+
+prec = ((10 + 5) * 3) // 15
+print(prec)
+
