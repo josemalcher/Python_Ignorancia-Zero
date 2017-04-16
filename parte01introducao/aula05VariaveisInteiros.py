5d14cdd4585de790465e33f9d47c5ca85d64295d
José Malcher Junior <contato@josemalcher.net>
2017-04-16 15:47:07 -0300

    Aulas Python - 005 - Variáveis, Inteiros

---
+++ parte01introducao/aula05VariaveisInteiros.py
@@ -0,0 +1,29 @@
+# Aulas Python - 005 - Variáveis, Inteiros
+# https://www.youtube.com/watch?v=27smIRZ6ZlI&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe&index=6
+from builtins import print
+
+print("Eu Tenho", 32, "Anos")
+
+print("Eu Tenho", 1, "Cachorro")
+
+num = 12
+print("Eu tenho ",num,"cachorros")
+
+a = 1
+b = 2
+
+print(a,b)
+
+a = b
+print(a,b)
+
+a = b = 30
+print(a,b)
+
+x , y = 20, 50
+print(x,y)
+
+ação = "variavel com acento verção 2 pra frente"
+print(ação)
+
+
