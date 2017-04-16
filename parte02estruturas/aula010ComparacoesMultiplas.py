4dfc737136a80d24df543e5a84776ea7abd4e6b2
José Malcher Junior <contato@josemalcher.net>
2017-04-16 19:10:31 -0300

    Aulas Python - 010 - Comparações Múltiplas

---
+++ parte02estruturas/aula010ComparacoesMultiplas.py
@@ -0,0 +1,13 @@
+# Aulas Python - 010 - Comparações Múltiplas
+# https://www.youtube.com/watch?v=_9D4S4ZMnzQ&list=PLfCKf0-awunOu2WyLe2pSD2fXUo795xRe&index=11&spfreload=1
+
+
+
+
+idade = int(input("Digite sua Idade "))
+if 18 <= idade < 70:
+    print("Voce pode receber o Beneficio")
+else:
+    print("Voce NÂO PODE receber")
+
+
